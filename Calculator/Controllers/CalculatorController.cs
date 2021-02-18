19377cd45430f31cdf8ba7404024494b5e822bcf
Piravin Satkunarajah <piravin.satkunarajah@ofgem.gov.uk>
2021-02-18 11:38:48 +0000

    Add project files.

---
+++ Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: /Calculator/
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Calculator/Welcome/
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method...";
+        //}
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        //}
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
