0a57dad0e6a7a27e31239ea84b6b2f9c6a300cd5
Piravin Satkunarajah <piravin.satkunarajah@ofgem.gov.uk>
2021-02-19 14:39:45 +0000

    Added model for request

---
+++ Calculator/Models/Request.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Calculator.Models
+{
+    public class Request
+    {
+        //Primary Key
+        public int Id { get; set; }
+        public string IpAddress { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime RequestDateTime { get; set; }
+
+    }
+}
