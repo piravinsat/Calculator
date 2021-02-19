5a5e5d038fa1a714ff239991ccb94ed0bae4dc1e
Piravin Satkunarajah <piravin.satkunarajah@ofgem.gov.uk>
2021-02-19 15:03:08 +0000

    Added Request model

--- Calculator/Startup.cs
+++ Calculator/Startup.cs
@@ -29,6 +29,9 @@ namespace Calculator
 
             services.AddDbContext<MvcMovieContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
+
+            services.AddDbContext<RequestContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("RequestContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
