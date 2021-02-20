b2e171e5490e27f9013aba02441c98f34f841a5b
Piravin Satkunarajah <piravin.satkunarajah@ofgem.gov.uk>
2021-02-20 12:02:50 +0000

    Update Add method

--- Calculator.Tests/CalculatorServiceTests.cs
+++ Calculator.Tests/CalculatorServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.ComponentModel.DataAnnotations;
+using NUnit.Framework;
 using Calculator.Services;
 
 namespace Calculator.Tests
@@ -6,9 +7,13 @@ namespace Calculator.Tests
     [TestFixture]
     public class CalculatorServiceTests
     {
+        private ICalculatorService _service;
+
         [SetUp]
         public void Setup()
         {
+            //Arrange
+            _service = new CalculatorService();
         }
 
         [Test]
@@ -20,14 +25,61 @@ namespace Calculator.Tests
         [Test]
         public void Add_EmptyString_ReturnZero()
         {
-            //Arrange
-            var service = new CalculatorService();
-
             //Act
-            var result = service.Add("");
+            var result = _service.Add("");
 
             //Assert
             Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Add_0_ReturnZero()
+        {
+            //Act
+            var result = _service.Add("0");
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Add_1_ReturnOne()
+        {
+            //Act
+            var result = _service.Add("1");
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+
+
+        [TestCase("64", 64)]
+        [TestCase("30", 30)]
+        public void Add_Any_Number_Return(string numbers, int expected)
+        {
+            //Act
+            var result = _service.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_Any_Two_Numbers_Return()
+        {
+            //Act
+            var result = _service.Add("1,2");
+
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void Add_Any_Three_Numbers_Return()
+        {
+            var result = _service.Add("1,2,3");
+
+            Assert.AreEqual(6, result);
         }
     }
 }
--- Calculator/Services/CalculatorService.cs
+++ Calculator/Services/CalculatorService.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using static System.String;
+﻿using static System.String;
 
 namespace Calculator.Services
 {
@@ -9,11 +6,21 @@ namespace Calculator.Services
     {
         public int Add(string numbers)
         {
+            var count = 0;
             var result = 0;
 
             if (IsNullOrEmpty(numbers))
             {
-                return 0;
+                return result;
+            }
+
+            var listOfNumbers = numbers.Split(',');
+
+            while (count < listOfNumbers.Length)
+            {
+                int.TryParse(listOfNumbers[count], out int num);
+                result += num;
+                count++;
             }
 
             return result;
